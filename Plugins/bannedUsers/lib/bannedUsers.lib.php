6b8e9ffd0688b2ceb64a1248d63fd8c244818253
surveybuilderadmin <64079569+surveybuilder-admin@users.noreply.github.com>
2021-10-29 14:31:21 -0400

    Add files via upload

--- Plugins/bannedUsers/lib/bannedUsers.lib.php
+++ Plugins/bannedUsers/lib/bannedUsers.lib.php
@@ -1,7 +1,7 @@
 <?php
 function CheckBanned($user){
     $plugin = "bannedUsers";
-$dom = str_replace("https://surveybuilder.epizy.com", $_SERVER['DOCUMENT_ROOT'],HTML_PLUGIN_DIR) . $plugin . DS. "db". DS . $user;
+$dom = PLUGIN_DIR . $plugin . DS. "db". DS . $user;
   $getContent = file_get_contents($dom);
 $query = json_decode($getContent);
 if($query->isBanned === "on"){
