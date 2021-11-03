aa6cc90aec019c5938f03d69516bf489c46e70ce
surveybuilderadmin <64079569+surveybuilder-admin@users.noreply.github.com>
2021-11-03 06:44:15 -0400

    Add files via upload

--- Plugins/bannedUsers/bannedUsers.plg.php
+++ Plugins/bannedUsers/bannedUsers.plg.php
@@ -29,7 +29,8 @@ function bannedUsers_install()
     $data['bannedMessage']           = '';
     $data['ip']                     = '';
     $data['appeal']                 = 'appeal/';
-    $data['isAdmin']                = str_replace("@", '', $sessionTrip);
+ $adminGetter = explode("@", $sessionTrip);
+                $data['isAdmin']        =  $adminGetter[0];
     
 	flatDB::saveEntry('plugin', $plugin, $data);
 }
@@ -47,7 +48,8 @@ function bannedUsers_config()
                $data['isBanned'] 		 = HTMLForm::clean($_POST['BannedCheck']);
                $data['bannedMessage']    = HTMLForm::clean($_POST['BanTxtarea']);
                $data['appealURI']        = HTMLForm::clean($_POST['appealURI']);
-                $data['isAdmin']        = str_replace("@", $sessionTrip);
+               $adminGetter = explode("@", $sessionTrip);
+                $data['isAdmin']        =  $adminGetter[0];
            
                
                $db = str_replace("https://surveybuilder.epizy.com", $_SERVER['DOCUMENT_ROOT'],HTML_PLUGIN_DIR) . $plugin . DS. "db". DS .$data['username'].".dat.json";
@@ -71,7 +73,7 @@ function bannedUsers_config()
               $url = $sec.$_SERVER['HTTP_HOST']."/".$data['appealURI'];
              
              
-              $image = scandir(PLUGIN_DIR.$plugin.DS."assets/img");
+              $image = scandir(PLUGIN_DIR.$plugin.DS."assets/imgs");
               foreach($image as $img){
                   if(!preg_match("/\.(gif|jpe?g|tiff?|png|webp|bmp)$/i", $img)){
                       $imgs = htmlspecialchars('<div class="alert alert-danger" role="alert">Error: '.$img.' is not a valid image type</div>', ENT_QUOTES);
@@ -92,6 +94,91 @@ function bannedUsers_config()
                   }
                  
               }
+            
+         
+
+              
+function getBrowser() { 
+  $u_agent = $_SERVER['HTTP_USER_AGENT'];
+  $bname = 'Unknown';
+  $platform = 'Unknown';
+  $version= "";
+  $device = "Unknown";
+  
+     
+  
+  //First get the platform?
+  if (preg_match('/linux/i', $u_agent)) {
+    $platform = 'linux';
+  }elseif (preg_match('/macintosh|mac os x/i', $u_agent)) {
+    $platform = 'mac';
+  }elseif (preg_match('/windows|win32/i', $u_agent)) {
+    $platform = 'windows';
+  }
+
+  // Next get the name of the useragent yes seperately and for good reason
+  if(preg_match('/MSIE/i',$u_agent) && !preg_match('/Opera/i',$u_agent)){
+    $bname = 'Internet Explorer';
+    $ub = "MSIE";
+  }elseif(preg_match('/Firefox/i',$u_agent)){
+    $bname = 'Mozilla Firefox';
+    $ub = "Firefox";
+  }elseif(preg_match('/OPR/i',$u_agent)){
+    $bname = 'Opera';
+    $ub = "Opera";
+  }elseif(preg_match('/Chrome/i',$u_agent) && !preg_match('/Edge/i',$u_agent)){
+    $bname = 'Google Chrome';
+    $ub = "Chrome";
+  }elseif(preg_match('/Safari/i',$u_agent) && !preg_match('/Edge/i',$u_agent)){
+    $bname = 'Apple Safari';
+    $ub = "Safari";
+  }elseif(preg_match('/Netscape/i',$u_agent)){
+    $bname = 'Netscape';
+    $ub = "Netscape";
+  }elseif(preg_match('/Edge/i',$u_agent)){
+    $bname = 'Edge';
+    $ub = "Edge";
+  }elseif(preg_match('/Trident/i',$u_agent)){
+    $bname = 'Internet Explorer';
+    $ub = "MSIE";
+  }
+
+  // finally get the correct version number
+  $known = array('Version', $ub, 'other');
+  $pattern = '#(?<browser>' . join('|', $known) .
+')[/ ]+(?<version>[0-9.|a-zA-Z.]*)#';
+  if (!preg_match_all($pattern, $u_agent, $matches)) {
+    // we have no matching number just continue
+  }
+  // see how many we have
+  $i = count($matches['browser']);
+  if ($i != 1) {
+    //we will have two since we are not using 'other' argument yet
+    //see if version is before or after the name
+    if (strripos($u_agent,"Version") < strripos($u_agent,$ub)){
+        $version= $matches['version'][0];
+    }else {
+        $version= $matches['version'][1];
+    }
+  }else {
+    $version= $matches['version'][0];
+  }
+
+  // check if we have a number
+  if ($version==null || $version=="") {$version="?";}
+
+  return array(
+    'userAgent' => $u_agent,
+    'name'      => $bname,
+    'version'   => $version,
+    'platform'  => $platform,
+    'pattern'    => $pattern
+  );
+} 
+
+$getAgent = getBrowser();
+
+               
              
                
                
@@ -102,7 +189,11 @@ function bannedUsers_config()
                 $data['bannedMessage'] = str_replace("{{GET_DATE}}", date("m-d-Y"), $data['bannedMessage']);
                 $data['bannedMessage'] = str_replace("{{GET_TIME}}", date("h:i:sa"), $data['bannedMessage']);
                 $data['bannedMessage'] = str_replace("{{GET_IP}}", $data['ip'], $data['bannedMessage']);
-                $data['bannedMessage'] = str_replace("{{GET_BROWSER}}",$_SERVER['HTTP_USER_AGENT'] , $data['bannedMessage']);
+                $data['bannedMessage'] = str_replace("{{GET_BROWSER}}", $getAgent['name'] , $data['bannedMessage']);
+                $data['bannedMessage'] = str_replace("{{GET_USERAGENT}}", $getAgent['userAgent'] , $data['bannedMessage']);
+                $data['bannedMessage'] = str_replace("{{GET_VERSION}}", $getAgent['version'] , $data['bannedMessage']);
+                $data['bannedMessage'] = str_replace("{{GET_PLATFORM}}", $getAgent['platform'] , $data['bannedMessage']);
+             
                 $data['bannedMessage'] = str_replace("{{GET_APPEAL_LINK}}", $url, $data['bannedMessage']);
                 $data['bannedMessage'] = str_replace("{{POST_IMAGES}}", $imgs, $data['bannedMessage']);
                 $data['bannedMessage'] = str_replace("{{POST_VIDEOS}", $vids, $data['bannedMessage']);
@@ -111,7 +202,7 @@ function bannedUsers_config()
                 $createFile = fopen($dom, "w+");
                 $data['bannedMessage'] = str_replace(array("\r","\n"), "", $data['bannedMessage']);
                
-                    fwrite($createFile, '{"username":"'.$data['username'].'", "isBanned":"'.$data['isBanned'].'", "bannedMessage": "'.$data['bannedMessage'].'", "ip":"'.$data['ip'].'"}');
+                    fwrite($createFile, '{"username":"'.$data['username'].'", "isBanned":"'.$data['isBanned'].'", "bannedMessage": "'.$data['bannedMessage'].'", "ip":"'.$data['ip'].'", "status": "pending", "timeStamp":"'.date('Y-m-d').'"}');
                     fclose($createFile);
                    
                 
@@ -181,26 +272,45 @@ function bannedUsers_config()
                );
        }
        $out.="<span id='panelbtn' onclick='showDisplayer()' data-toggle='tooltip' data-placement='top' title='Open Panel'>Open Panel</span>";
-       $out.="<span id='displayCon'><div class='closebtn' onclick='hideDisplayer()'><i class='fas fa-times-circle' title='Close'></i> Close</div>";
+       $out.="<span id='displayCon'><div class='closebtn' onclick='hideDisplayer()' data-toggle='tooltip' data-placement='top' title='Close Panel'><i class='fas fa-times-circle'></i> Close Panel</div>";
  $getDom = PLUGIN_DIR . $plugin . DS. "db". DS;
                     $files = glob($getDom."*.dat.json");
                    
-                
+
                
          
                   
-                    foreach($files as $fkey => $file){
+                    foreach($files as $file){
                       
                      $replace = str_replace($getDom,"",$file);
                     
-         
+                       
+                    if(isFailed($replace)){
+                $out.= "<div class='alert alert-danger' role='alert'><i class='fas fa-times-circle'></i> Failed user... ".$replace." <a href='".str_replace($_SERVER["DOCUMENT_ROOT"],"",$getDom)."status.php?delete=".$replace."'><i class='fas fa-user-times title='Delete User' style='color:red;'></i></a> | <a href='".str_replace($_SERVER["DOCUMENT_ROOT"],"",$getDom)."status.php?repend=".$replace."'><i class='fas fa-sync-alt' title='Repend' style='color:blue;'></i></a></div>"; 
+                    }else{
+       
+                        if(isPending($replace)){
+                            if(!checkPending($replace)){
+                                    $plugin = 'bannedUsers';
+ $dom = PLUGIN_DIR . $plugin . DS. "db". DS . $replace;
+           $createFile = fopen($dom, "w+");
+                    fwrite($createFile, '{"username":"'.$replace.'", "isBanned":"", "bannedMessage": "", "ip": "", "status": "failed", "timeStamp": ""}');
+                    fclose($createFile);
+                       $out.= "<div class='alert alert-danger' role='alert'><i class='fas fa-times-circle'></i> ".$lang['pendFail']." ".$replace." <a href='".str_replace($_SERVER["DOCUMENT_ROOT"],"",$getDom)."status.php?delete=".$replace."'><i class='fas fa-user-times' title='Delete User' style='color:red;'></i></a> | <a href='".str_replace($_SERVER["DOCUMENT_ROOT"],"",$getDom)."status.php?repend=".$replace."'><i class='fas fa-sync-alt' title='Repend' style='color:blue;'></i></a></div>"; 
+                            }else{
+                                $getPend = pendingExpire($replace);
+                        $out.= "<div class='alert alert-warning' role='alert'><i class='fas fa-exclamation-triangle'></i> ".$lang['pend']  ." ".$replace." ".$lang['pendExpire']  ." ".$getPend." <a href='".str_replace($_SERVER["DOCUMENT_ROOT"],"",$getDom)."status.php?delete=".$replace."'><i class='fas fa-user-times' title='Cancel Pending' style='color:red;'></i></a></div>"; 
+                            }
                     
-              
-                         if(!CheckBanned($replace)){
+                        }else{
+                            if(!CheckBanned($replace)){
                            $out.= "<div class='alert alert-success' role='alert'><i class='fas fa-check-circle'></i> ".$replace." <a href='".str_replace($_SERVER["DOCUMENT_ROOT"],"",$getDom)."status.php?ban=".$replace."'><i class='fas fa-user-unlock' title='Ban' style='color:green;'></i></a></div>"; 
                         }else{
 $out.= "<div class='alert alert-danger' role='alert'><i class='fas fa-times-circle'></i> ".$replace." <a href='".str_replace($_SERVER["DOCUMENT_ROOT"],"",$getDom)."status.php?unban=".$replace."'><i class='fas fa-user-lock' title='Unban' style='color:red;'></i></a></div>"; 
                             }
+                        }
+              
+                    }
 
                         
                     
@@ -243,7 +353,7 @@ $out.= "<div class='alert alert-danger' role='alert'><i class='fas fa-times-circ
          }
          </style>";
          if($sessionTrip){
-             CreateUser($sessionTrip,'','', '', User::getRealIpAddr());
+             CreateUser($sessionTrip,'','', User::getRealIpAddr(), 'active', date('Y-m-d'));
          }
      }
       
--- Plugins/bannedUsers/createFileSession.php
+++ Plugins/bannedUsers/createFileSession.php
@@ -1,11 +1,19 @@
 <?php
-function CreateUser($user, $isBanned="", $bannedMessage="", $whiteListed="", $ip){
+function CreateUser($user, $isBanned="", $bannedMessage="", $ip, $status="active", $timeStamp){
     $plugin = 'bannedUsers';
- $dom = str_replace("https://surveybuilder.epizy.com", $_SERVER['DOCUMENT_ROOT'],HTML_PLUGIN_DIR) . $plugin . DS. "db". DS . $user.".dat.json";
+ $dom = PLUGIN_DIR . $plugin . DS. "db". DS . $user.".dat.json";
        if(!file_exists($dom)){
            $createFile = fopen($dom, "w+");
-                    fwrite($createFile, '{"username":"'.$user.'", "isBanned":"'.$isBanned.'", "bannedMessage": "'.$bannedMessage.'", "ip": "'.$ip.'", "whitelisted": "'.$whiteListed.'"}');
+                    fwrite($createFile, '{"username":"'.$user.'", "isBanned":"'.$isBanned.'", "bannedMessage": "'.$bannedMessage.'", "ip": "'.$ip.'", "status": "'.$status.'", "timeStamp": "'.$timeStamp.'"}');
                     fclose($createFile);
+       }else{
+           $check = file_get_contents($dom);
+           $query = json_decode($check);
+           if($query->status === "pending"){
+               $createFile = fopen($dom, "w+");
+                    fwrite($createFile, '{"username":"'.$user.'", "isBanned":"'.$isBanned.'", "bannedMessage": "'.$bannedMessage.'", "ip": "'.$ip.'", "status": "'.$status.'", "timeStamp": "'.$timeStamp.'"}');
+                    fclose($createFile);
+           }
        }
 }
 ?>
