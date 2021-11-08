4bc7f93ed869ec7435bd64ea763d315a83a7f955
surveybuilderadmin <64079569+surveybuilder-admin@users.noreply.github.com>
2021-11-08 08:12:31 -0500

    Add files via upload

--- Plugins/page/lang/en-US.php
+++ Plugins/page/lang/en-US.php
@@ -1,12 +1,12 @@
 <?php
 /************* Plugin Info REQUIRED! ***************/
 $lang[$plugin.'name']           = 'Pages';
-$lang[$plugin.'version']        = '1.0.0';
-$lang[$plugin.'update']         = '2021-11-05';
+$lang[$plugin.'version']        = '1.0.1';
+$lang[$plugin.'update']         = '2021-11-08';
 $lang[$plugin.'author']         = 'SurveyBuilder-Admin';
 $lang[$plugin.'author_site']    = 'https://github.com/surveybuilderteams/Flatboard-Plugins';
 $lang[$plugin.'author_mail']    = 'surveybuildersbot@gmail.com';
 /************* Language  ***************/
 $lang[$plugin.'description']    = 'Allows you to make custom pages to the forum';
- 
+
 ?>
