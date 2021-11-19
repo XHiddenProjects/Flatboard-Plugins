7096e11e015e4e54dca68265ce0759980935acb1
surveybuilderadmin <64079569+surveybuilder-admin@users.noreply.github.com>
2021-11-19 09:43:03 -0500

    Add files via upload

--- Plugins/bannedUsers/lang/es-ES.php
+++ Plugins/bannedUsers/lang/es-ES.php
@@ -1,8 +1,8 @@
 <?php
 /************* Plugin Info REQUIRED! ***************/
 $lang[$plugin.'name']           = 'Banned User';
-$lang[$plugin.'version']        = '1.2.2';
-$lang[$plugin.'update']         = '2021-11-04';
+$lang[$plugin.'version']        = '1.2.3';
+$lang[$plugin.'update']         = '2021-11-19';
 $lang[$plugin.'author']         = 'SurveyBuilder-Admin';
 $lang[$plugin.'author_site']    = 'https://github.com/surveybuilderteams/Flatboard-Plugins';
 $lang[$plugin.'author_mail']    = 'surveybuildersbot@gmail.com';
