6a3d088c385a571af159694ad3a08e218f493265
surveybuilder-admin <64079569+surveybuilder-admin@users.noreply.github.com>
2021-12-18 07:57:51 -0500

    Add files via upload

---
+++ Plugins/Notification/lang/en-US.php
@@ -0,0 +1,11 @@
+<?php
+/************* Plugin Info REQUIRED! ***************/
+$lang[$plugin.'name']           = 'Notification';
+$lang[$plugin.'version']        = '1.0.12';
+$lang[$plugin.'update']         = '2021-12-15';
+$lang[$plugin.'author']         = 'SurveyBuilder-Admin';
+$lang[$plugin.'author_site']    = 'https://github.com/surveybuilderteams/Flatboard-Plugins';
+$lang[$plugin.'author_mail']    = 'surveybuildersbot@gmail.com';
+/************* Language  ***************/
+$lang[$plugin.'description']    = 'Recive new notifcation when your out, and not need spam emails';
+?>
