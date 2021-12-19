59002b1fba2968f4a69e12fa91e30dca8ef19747
surveybuilder-admin <64079569+surveybuilder-admin@users.noreply.github.com>
2021-12-19 07:08:34 -0500

    Update en-US.php

--- Plugins/Notification/lang/en-US.php
+++ Plugins/Notification/lang/en-US.php
@@ -1,8 +1,8 @@
 <?php
 /************* Plugin Info REQUIRED! ***************/
 $lang[$plugin.'name']           = 'Notification';
-$lang[$plugin.'version']        = '1.0.12';
-$lang[$plugin.'update']         = '2021-12-15';
+$lang[$plugin.'version']        = '2.0.0';
+$lang[$plugin.'update']         = '2021-12-19';
 $lang[$plugin.'author']         = 'SurveyBuilder-Admin';
 $lang[$plugin.'author_site']    = 'https://github.com/surveybuilderteams/Flatboard-Plugins';
 $lang[$plugin.'author_mail']    = 'surveybuildersbot@gmail.com';
