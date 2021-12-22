bdb85f4d9e5e77b6bf5008beee5b5d705aa4b492
surveybuilder-admin <64079569+surveybuilder-admin@users.noreply.github.com>
2021-12-22 07:45:55 -0500

    Add files via upload

--- Plugins/page/lang/en-US.php
+++ Plugins/page/lang/en-US.php
@@ -1,6 +1,6 @@
 <?php
 /************* Plugin Info REQUIRED! ***************/
-$lang[$plugin.'name']           = 'Pages';
+$lang[$plugin.'name']           = 'Page Manager';
 $lang[$plugin.'version']        = '1.0.1';
 $lang[$plugin.'update']         = '2021-11-08';
 $lang[$plugin.'author']         = 'SurveyBuilder-Admin';
@@ -8,5 +8,9 @@ $lang[$plugin.'author_site']    = 'https://github.com/surveybuilderteams/Flatboa
 $lang[$plugin.'author_mail']    = 'surveybuildersbot@gmail.com';
 /************* Language  ***************/
 $lang[$plugin.'description']    = 'Allows you to make custom pages to the forum';
-
+$lang['pagePlace'] = 'Enter Page';
+$lang['WriteCode'] = 'Enter code';
+$lang['selDef'] = '--Select Page--';
+$lang['NewFile'] = 'Enter New File(Leave blank if not in use)';
+$lang['DeletePage'] = "Delete Page";
 ?>
