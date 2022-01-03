061212a2d7f5b8091efd13877d561747a263be91
surveybuilder-admin <64079569+surveybuilder-admin@users.noreply.github.com>
2022-01-03 18:45:35 -0500

    Add files via upload

--- Plugins/Notification/lang/it-IT.php
+++ Plugins/Notification/lang/it-IT.php
@@ -1,8 +1,8 @@
 <?php
 /************* Plugin Info REQUIRED! ***************/
 $lang[$plugin.'name']           = 'Notifica';
-$lang[$plugin.'version']        = '2.0.0';
-$lang[$plugin.'update']         = '2021-12-19';
+$lang[$plugin.'version']        = '2.2.0';
+$lang[$plugin.'update']         = '2022-01-03';
 $lang[$plugin.'author']         = 'SurveyBuilder-Admin';
 $lang[$plugin.'author_site']    = 'https://github.com/surveybuilderteams/Flatboard-Plugins';
 $lang[$plugin.'author_mail']    = 'surveybuildersbot@gmail.com';
