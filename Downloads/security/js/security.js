c1211debeb104283b95cacc6076698debed3802e
Gavin Zeager <64079569+XHiddenProjects@users.noreply.github.com>
2025-02-09 13:58:36 -0500

    Delete Downloads/security directory

--- Downloads/security/js/security.js
+++
@@ -1,5 +0,0 @@
-import xss from "./xss.min.js"
-window.addEventListener('load',()=>{
-    const observer = new MutationObserver(() => {
-        xss.sanitize();});
-    observer.observe(document, {subtree: true,attributes: true,});});
